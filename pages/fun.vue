a69ed4326421e4c1f316df80668b92a62a401e4e
Paras M <64939659+pmxi@users.noreply.github.com>
2022-06-13 23:01:25 -0400

    c

---
+++ pages/fun.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>fun sentence</h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <Tutorial /> -->
+  <main>
+    <h1>Hello World</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
